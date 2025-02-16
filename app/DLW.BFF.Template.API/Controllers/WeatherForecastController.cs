018bf0ed21106c20e69baa99d90b49a8773eb4c1
Robin Agten <robin.agten@delaware.pro>
2025-02-16 09:50:56 +0100

    simplify weather data

--- app/DLW.BFF.Template.API/Controllers/WeatherForecastController.cs
+++ app/DLW.BFF.Template.API/Controllers/WeatherForecastController.cs
@@ -5,31 +5,18 @@ namespace DLW.BFF.Template.API.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+            return Enumerable
+                .Range(1, 5)
+                .Select(index => summaries[Random.Shared.Next(summaries.Length)])
+                .ToArray();
         }
     }
 }
